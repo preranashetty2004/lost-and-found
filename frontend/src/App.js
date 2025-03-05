50a602e62003f2be443fa8f4fe8debad89303a9b
preranashetty2004 <preranashetty2004@gmail.com>
2025-03-05 23:09:15 +0530

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <main>
+        <h1>Welcome to the Lost and Found Portal</h1>
+        <p>Use the navigation bar to report lost and found items.</p>
+      </main>
     </div>
   );
 }
 
 export default App;
+
